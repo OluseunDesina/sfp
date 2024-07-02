e8a1d8b63a4fea968802ce86110ff29621aefe3f
Adesina Oluwaseun <oadesina@secureidltd.com>
2024-07-02 15:42:59 +0100

    [BUG FIX]: removed angular-paystack

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: true,
   // prod
   // url: "http://178.128.32.169:8000/",
-  url: "https://bucca.secureid-digital.com.ng/",
+  // url: "https://bucca.secureid-digital.com.ng/",
+  url: "https://2c0e-41-58-57-134.ngrok-free.app/",
   // url: "https://live-bucca-v2.herokuapp.com/",
   // test
   // url: "https://bucca-v2.herokuapp.com/"
