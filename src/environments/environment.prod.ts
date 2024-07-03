3e41a68f4d2bd8335b4e655d3d9522999ca0448f
Adesina Oluwaseun <oadesina@secureidltd.com>
2024-07-03 21:22:50 +0100

    Mods

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   // prod
   // url: "http://178.128.32.169:8000/",
   // url: "https://bucca.secureid-digital.com.ng/",
-  url: "https://2c0e-41-58-57-134.ngrok-free.app/",
+  url: "https://da18-41-58-58-65.ngrok-free.app/",
   // url: "https://live-bucca-v2.herokuapp.com/",
   // test
   // url: "https://bucca-v2.herokuapp.com/"
